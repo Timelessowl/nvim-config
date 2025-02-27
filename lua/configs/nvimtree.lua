8ac1399a5a3d812fca44d96fcbe0f234c03a9f37
Timelessowl <k.punenko@gmail.com>
2025-02-27 13:52:34 +0500

    ref: nvimtreegit colors improved

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -1,35 +1,41 @@
+require("configs.nvimtreegit")
 return {
-  filters = { dotfiles = false, git_clean = false, no_buffer = false, git_ignored = false },
-  disable_netrw = true,
-  hijack_cursor = true,
-  sync_root_with_cwd = true,
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-  },
-  view = {
-    width = 30,
-    preserve_window_proportions = true,
-  },
-  renderer = {
-    root_folder_label = false,
-    highlight_git = true,
-    indent_markers = { enable = true },
-    icons = {
-      glyphs = {
-        default = "",
-        folder = {
-          default = "",
-          empty = "",
-          empty_open = "",
-          open = "",
-          symlink = "",
-        },
-        git = { unmerged = "" },
-      },
-    },
-  },
-  git = {
-    ignore = false,
-  },
+	filters = { dotfiles = false, git_clean = false, no_buffer = false, git_ignored = false },
+	disable_netrw = true,
+	hijack_cursor = true,
+	sync_root_with_cwd = true,
+	update_focused_file = {
+		enable = true,
+		update_root = false,
+	},
+	view = {
+		width = 30,
+		preserve_window_proportions = true,
+	},
+	renderer = {
+		root_folder_label = false,
+		highlight_git = true,
+		indent_markers = { enable = true },
+		icons = {
+			glyphs = {
+				default = "",
+				folder = {
+					default = "",
+					empty = "",
+					empty_open = "",
+					open = "",
+					symlink = "",
+				},
+				git = {
+					unmerged = "",
+					unstaged = "",
+					staged = "✓",
+					untracked = "",
+				},
+			},
+		},
+	},
+	git = {
+		ignore = false,
+	},
 }
---
+++ lua/configs/nvimtreegit.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_set_hl(0, "NvimTreeGitFileDirtyHL", { fg = "#FFC499" })
+vim.api.nvim_set_hl(0, "NvimTreeGitDirtyIcon", { fg = "#FFC499" })
+
+vim.api.nvim_set_hl(0, "NvimTreeGitFileIgnoredHL", { fg = "#808080" })
+vim.api.nvim_set_hl(0, "NvimTreeGitIgnoredIcon", { fg = "#808080" })
+
+vim.api.nvim_set_hl(0, "NvimTreeGitNewIcon", { fg = "#75FA8D" })
